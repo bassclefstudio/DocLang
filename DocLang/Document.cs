a4ae142a16d1e13dae58006ce978ae342ec13a6e
Llewella Spencer <bassclefstudio-dev@outlook.com>
2022-01-11 01:22:28 -0500

    Added DocLangParser for full XML documents and DI; Author struct added for author information.

--- DocLang/Document.cs
+++ DocLang/Document.cs
@@ -1,43 +1,92 @@
-﻿using BassClefStudio.DocLang.Base;
+﻿using BassClefStudio.DocLang.Content;
+using BassClefStudio.DocLang.Metadata;
+using BassClefStudio.DocLang.Parsing;
+using CommunityToolkit.Diagnostics;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BassClefStudio.DocLang
 {
     /// <summary>
     /// The in-memory representation of a full document written in DocLang.
     /// </summary>
-    public class Document : CollectionNode
+    public class Document : Heading
     {
-        /// <inheritdoc/>
-        public override string NodeType { get; } = "Document";
+        /// <summary>
+        /// The XML/<see cref="IDocNode.NodeType"/> type for <see cref="Document"/>s.
+        /// </summary>
+        public new const string Type = "Document";
+
+        /// <summary>
+        /// The list of all <see cref="Author"/>s that were involved in the creation of the <see cref="Document"/>.
+        /// </summary>
+        public List<Author> Authors { get; }
 
         /// <summary>
         /// Creates a new, empty <see cref="Document"/>.
         /// </summary>
-        public Document() : base(null)
-        { }
+        /// <inheritdoc/>
+        public Document(string id, string name, IDocNode title) : base(id, name, title, Type)
+        {
+            Authors = new List<Author>();
+        }
+    }
+
+    /// <summary>
+    /// An <see cref="IDocParser"/> for the <see cref="Document"/> node (based on <see cref="HeadingParser"/>).
+    /// </summary>
+    public class DocumentParser : DocParser
+    {
+        /// <summary>
+        /// An <see cref="IParser{T}"/> for <see cref="Author"/> objects.
+        /// </summary>
+        public IParser<Author>? AuthorParser { get; set; }
 
         /// <summary>
-        /// Attempts to retrieve the <typeparamref name="T"/> value of the attribute stored in the <see cref="IDocMetadataNode{T}"/> with the given key.
+        /// The injected <see cref="ILogger"/>.
         /// </summary>
-        /// <typeparam name="T">The type of metadata to query for.</typeparam>
-        /// <param name="key">The <see cref="IDocMetadataNode{T}.Key"/> of the desired metadata item.</param>
-        /// <returns>The <typeparamref name="T"/> value if the <see cref="IDocMetadataNode{T}"/> was found; returns a default value otherwise.</returns>
-        public T? GetAttribute<T>(string key)
+        public ILogger<DocumentParser>? Logger { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="DocumentParser"/>.
+        /// </summary>
+        public DocumentParser() : base(Document.Type)
+        { }
+
+        /// <inheritdoc/>
+        public override IDocNode Read(XElement element)
         {
-            var item = Children.OfType<IDocMetadataNode<T>>().FirstOrDefault(data => data.Key == key);
-            if (item is not null)
+            Guard.IsNotNull(ChildParsers, nameof(ChildParsers));
+            Guard.IsNotNull(AuthorParser, nameof(AuthorParser));
+            Document document = new Document(element.GetAttribute("Id").Value, element.GetAttribute("Name").Value, ChildParsers.Read(element.GetElement("Title")));
+            document.Authors.AddRange(element.Elements("Author").Select(AuthorParser.Read));
+            document.ReadContent(element.GetElement("Content"), ChildParsers, Logger);
+            return document;
+        }
+
+        /// <inheritdoc/>
+        public override XNode Write(IDocNode node)
+        {
+            Guard.IsNotNull(ChildParsers, nameof(ChildParsers));
+            Guard.IsNotNull(AuthorParser, nameof(AuthorParser));
+            Guard.IsAssignableToType<Document>(node, nameof(node));
+            Document document = (Document)node;
+            XElement element = new XElement(Document.Type);
+            element.SetAttributeValue("Id", document.Id);
+            element.SetAttributeValue("Name", document.Name);
+            element.Add(ChildParsers.Write(document.Title));
+            foreach (var author in document.Authors)
             {
-                return item.Value;
+                element.Add(AuthorParser.Write(author));
             }
-            else
-            {
-                return default;
-            }
+            element.CreateElement("Content").Add(document.WriteContent(ChildParsers, Logger));
+            return element;
         }
     }
 }
---
+++ DocLang/Metadata/Author.cs
@@ -0,0 +1,81 @@
+﻿using BassClefStudio.DocLang.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BassClefStudio.DocLang.Metadata
+{
+    /// <summary>
+    /// A representation of a person who contributed to part of a <see cref="Document"/>.
+    /// </summary>
+    public struct Author
+    {
+        /// <summary>
+        /// The status/position of this author relative to the <see cref="Document"/> the author reference is attached to.
+        /// </summary>
+        public AuthorType Type { get; }
+
+        /// <summary>
+        /// The full name of the <see cref="Author"/>, as a <see cref="string"/>.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="Author"/> reference.
+        /// </summary>
+        /// <param name="type">The status/position of this author relative to the <see cref="Document"/> the author reference is attached to.</param>
+        /// <param name="name">The full name of the <see cref="Author"/>, as a <see cref="string"/>.</param>
+        public Author(AuthorType type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+
+    /// <summary>
+    /// A description of the status of a given <see cref="Author"/>.
+    /// </summary>
+    public enum AuthorType
+    {
+        /// <summary>
+        /// This <see cref="Author"/> created this <see cref="Document"/>, and/or was the primary contributor.
+        /// </summary>
+        Creator = 0,
+
+        /// <summary>
+        /// This <see cref="Author"/> aided in the creation/writing of this <see cref="Document"/>.
+        /// </summary>
+        Contributor = 1,
+
+        /// <summary>
+        /// This <see cref="Author"/> helped proofread, edit, or otherwise revise the final document.
+        /// </summary>
+        Editor = 2,
+
+        /// <summary>
+        /// This <see cref="Author"/> is responsible for publishing the completed document.
+        /// </summary>
+        Publisher = 3
+    }
+
+    public class AuthorParser : IParser<Author>
+    {
+        /// <inheritdoc/>
+        public Author Read(XElement element)
+        {
+            return new Author(Enum.Parse<AuthorType>(element.GetAttribute("Type").Value), element.Value);
+        }
+
+        /// <inheritdoc/>
+        public XNode Write(Author node)
+        {
+            XElement element = new XElement("Author");
+            element.SetAttributeValue("Type", node.Type.ToString());
+            element.Value = node.Name;
+            return element;
+        }
+    }
+}
