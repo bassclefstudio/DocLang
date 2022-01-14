5ab79bc1672eea851b3e8978b2567d4baecbac0f
Llewella Spencer <bassclefstudio-dev@outlook.com>
2022-01-13 22:54:28 -0500

    Added a strongly-typed DocParserService<T, TData> equivalent.

--- Serialization.Natural/INaturalSerializerService.cs
+++ Serialization.Natural/INaturalSerializerService.cs
@@ -34,4 +34,59 @@ namespace BassClefStudio.NET.Serialization.Natural
         /// <returns>A <see cref="bool"/> indicating whether the <see cref="INaturalSerializerService{T, TData}"/> was able to parse any of the provided data.</returns>
         bool Write(T content, TData data);
     }
+
+    /// <summary>
+    /// A base implementation of <see cref="INaturalSerializerService{T, TData}"/> with support for strongly-typed inputs.
+    /// </summary>
+    /// <typeparam name="T">The type of .NET objects being serialized and deserialized.</typeparam>
+    /// <typeparam name="TActual">The actual derived type of <typeparamref name="T"/> objecs this <see cref="INaturalSerializerService{T, TData}"/> supports.</typeparam>
+    /// <typeparam name="TData">The type of the data used to represent <typeparamref name="T"/> instances.</typeparam>
+    /// <typeparam name="TDataActual">The actual derived type of <typeparamref name="TData"/> objecs this <see cref="INaturalSerializerService{T, TData}"/> supports.</typeparam>
+    public abstract class NaturalSerializerService<T, TActual, TData, TDataActual> : INaturalSerializerService<T, TData> where TActual : T where TDataActual : TData
+    {
+        /// <inheritdoc/>
+        public uint Priority { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="NaturalSerializerService{T, TActual, TData, TDataActual}"/> with the given priority.
+        /// </summary>
+        /// <param name="priority">A <see cref="uint"/> priority which defines in which order <see cref="INaturalSerializerService{T, TData}"/>s should read and write content.</param>
+        public NaturalSerializerService(uint priority = 0)
+        {
+            Priority = priority;
+        }
+
+        /// <inheritdoc/>
+        public bool Read(T content, TData data)
+        {
+            if (content is TActual actualContent && data is TDataActual actualData)
+            {
+                return ReadInternal(actualContent, actualData);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <inheritdoc cref="Read(T, TData)"/>
+        protected abstract bool ReadInternal(TActual content, TDataActual data);
+
+        /// <inheritdoc/>
+        public bool Write(T content, TData data)
+        {
+            if (content is TActual actualContent && data is TDataActual actualData)
+            {
+                return WriteInternal(actualContent, actualData);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <inheritdoc cref="Write(T, TData)"/>
+        protected abstract bool WriteInternal(TActual content, TDataActual data);
+
+    }
 }
